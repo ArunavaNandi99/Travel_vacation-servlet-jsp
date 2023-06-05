674c028413c1cb1af0b74433da7b5f8ad0b168b3
Arunava_Nandi <arunavanandi99@gmail.com>
2023-06-05 20:09:10 +0530

    voltage integration

--- src/main/java/com/project1/controller/voltage/DecryptData.java
+++ src/main/java/com/project1/controller/voltage/DecryptData.java
@@ -3,21 +3,19 @@ package com.project1.controller.voltage;
 import com.project1.service.VoltageService;
 import com.project1.service.VoltageServiceImpl;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
 
 @WebServlet("/decrypt")
 public class DecryptData extends HttpServlet {
 
     VoltageService voltageService = new VoltageServiceImpl();
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
 
         String phoneNumber = request.getParameter("phoneNumber");
 
@@ -32,8 +30,6 @@ public class DecryptData extends HttpServlet {
             session.setAttribute("data", fakeData);
 
             response.sendRedirect("/project1/showData");
-//            RequestDispatcher dispatcher = request.getRequestDispatcher("/showData");
-//            dispatcher.forward(request,response);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }

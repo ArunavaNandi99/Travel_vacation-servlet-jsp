42012cebbee953cfb5009b1950f4c54964b219ce
Arunava_Nandi <arunavanandi99@gmail.com>
2023-06-05 20:09:27 +0530

    voltage integration

--- src/main/java/com/project1/controller/voltage/DecryptData.java
+++ src/main/java/com/project1/controller/voltage/DecryptData.java
@@ -3,7 +3,6 @@ package com.project1.controller.voltage;
 import com.project1.service.VoltageService;
 import com.project1.service.VoltageServiceImpl;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
